657e040256075f2655ae98edc48af54e0e56d64f
eonjugg <eonjugg@gmail.com>
2025-02-02 00:59:55 -0500

    fix bug + clean code

--- pf/pf.js
+++ pf/pf.js
@@ -23,18 +23,17 @@ for (let y = 0; y < gridheight; y++) {
     grid.push(row);
 }
 
-path();
+draw();
 
 function path() {
     // flood fill / BFS
-    // might need to actually implement Dijkstra's or A* if canvas size is big, but i feel whatever this is
-    // is faster for smaller sizes like 20x30 since it uses a hashmap
+    // can be called separate from draw() to see if current grid configuration has a solution path (spath.length > 0)
     let visited = new Map();
-    let fringe = Array({ x: 0, y: 0, len: 0, prev: undefined });
+    let fringe = Array({ x: 0, y: 0, dist: 0, prev: undefined });
     let found = false;
     let last;
     while (fringe.length > 0) {
-        fringe.sort((a, b) => b.len - a.len); // honestly not horrible performance from this, ~28k total comparisons at most for 20x30 grid
+        fringe.sort((a, b) => b.dist - a.dist); // honestly not horrible performance from this, ~28k total comparisons at most for 20x30 grid
         let top;
         do {
             top = fringe.pop();
@@ -49,28 +48,28 @@ function path() {
             fringe.push({
                 x: top.x - 1,
                 y: top.y,
-                len: top.len + 1,
+                dist: top.dist + 1,
                 prev: top,
             });
         if (top.x < gridwidth - 1 && visited[[top.y, top.x + 1]] != true && grid[top.y][top.x + 1] == true)
             fringe.push({
                 x: top.x + 1,
                 y: top.y,
-                len: top.len + 1,
+                dist: top.dist + 1,
                 prev: top,
             });
         if (top.y > 0 && visited[[top.y - 1, top.x]] != true && grid[top.y - 1][top.x] == true)
             fringe.push({
                 x: top.x,
                 y: top.y - 1,
-                len: top.len + 1,
+                dist: top.dist + 1,
                 prev: top,
             });
         if (top.y < gridheight - 1 && visited[[top.y + 1, top.x]] != true && grid[top.y + 1][top.x] == true)
             fringe.push({
                 x: top.x,
                 y: top.y + 1,
-                len: top.len + 1,
+                dist: top.dist + 1,
                 prev: top,
             });
     }
@@ -81,16 +80,14 @@ function path() {
             spath.push(last);
             last = last.prev;
         } while (!(last.x == 0 && last.y == 0));
-        document.getElementById("pathlengthlabel").innerText = "Length: " + spath.length;
     } else {
         spath = Array();
         pathexists = false;
-        document.getElementById("pathlengthlabel").innerText = "Path obstructed";
     }
-    draw();
 }
 
 function draw() {
+    path();
     if (pathexists == true) {
         ctx.fillStyle = "white";
     } else {
@@ -122,6 +119,11 @@ function draw() {
         ctx.lineTo(hgss, hgss);
         ctx.stroke();
     }
+    if (spath.length > 0) {
+        document.getElementById("pathlengthlabel").innerText = "Length: " + spath.length;
+    } else {
+        document.getElementById("pathlengthlabel").innerText = "Path obstructed";
+    }
 }
 
 function cleargrid() {
@@ -130,37 +132,46 @@ function cleargrid() {
             grid[y][x] = true;
         }
     }
-    path();
+    draw();
 }
 
 function randomsquare() {
-    let maxattempts = 100;
+    let maxattempts = 150;
     let attempts = 0;
     for (let n = 0; n < 5 && attempts < maxattempts; n++) {
         let x, y;
         do {
             y = Math.floor(Math.random() * gridheight);
             x = Math.floor(Math.random() * gridwidth);
-            attempts += 1;
-        } while (attempts < maxattempts && grid[y][x] !== true && x === target[1] && y === target[0]);
+            attempts += 1;                  // keep randomly picking a square in the grid while:
+        } while (attempts < maxattempts && (    // we haven't reached attempt limit, AND
+            grid[y][x] === false ||                 // random pick is already a wall (non-traversable),
+            (x === target[1] && y === target[0]) || // the target square,
+            (x === 0 && y === 0)                    // or the starting square
+        ));
         if (attempts < maxattempts) {
             grid[y][x] = false;
+            path();
+            if (spath.length === 0) {
+                grid[y][x] = true; // don't block this square if it would make grid have no solution path
+                n--; // also don't count this pick as part of the amount we want
+            }
         }
     }
     if (attempts === maxattempts) {
         console.log("Max attempt limit " + maxattempts + " reached for randomsquare()");
     }
-    path();
+    draw();
 }
 
 function drawsquare(canvas, event) {
     let rect = canvas.getBoundingClientRect();
     let x = Math.floor((event.clientX - rect.left) / gridsquaresize);
     let y = Math.floor((event.clientY - rect.top) / gridsquaresize);
-    if (!(x === target[1] && y === target[0])) {
+    if (!(x === target[1] && y === target[0]) && !(x === 0 && y === 0)) {
         grid[y][x] = !grid[y][x];
     }
-    path();
+    draw();
 }
 
 cv.addEventListener("mousedown", function (e) {
