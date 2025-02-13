30ad43928348bbf3f5d59d624febf03d469cb324
eonjugg <eonjugg@gmail.com>
2025-02-13 14:14:36 -0500

    good start point

--- utnitled_game/game.js
+++ utnitled_game/game.js
@@ -1,6 +1,25 @@
+/* Heavily inspired by Trimps https://trimps.github.io/ */
+const stat_numrobots = document.getElementById("stat_numrobots")
+const pb_buildrobot = document.getElementById("pb_buildrobot")
+let buildingrobot = false
+const stats = {
+    "robots": 0,
+}
 
+function buildrobot() {
+    if (!buildingrobot) {
+        buildingrobot = true
+        let time = 3 // seconds
+        pb_buildrobot.style.transitionDuration = `${time}s`
+        pb_buildrobot.setAttribute("class", "a") // Set class "a" ( for 'active' )
+        setTimeout(buildrobotcomplete, time*1000) // Do thing indicated by end of progress bar when animation completes
+    }
+}
 
-
-function startfight() {
-    console.log("Fight")
+function buildrobotcomplete() {
+    pb_buildrobot.style.transitionDuration = "0s" // Instantly reset progress bar
+    pb_buildrobot.setAttribute("class", "")
+    stats.robots += 1
+    stat_numrobots.innerText = stats.robots
+    buildingrobot = false
 }
